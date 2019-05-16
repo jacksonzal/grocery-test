53b58ada0b4689950f8deaca4ce56048546072a8
Jackson Zaloumis <j.g.zaloumis@gmail.com>
2019-05-16 14:51:22 +1000

    Add react-testing-library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { MemoryRouter } from "react-router-dom";
+
+import { render } from "react-testing-library";
+
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(container).toBeDefined();
 });
--- src/domains/add/pages/index.tsx
+++ src/domains/add/pages/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Add() {
-  return <div>Hello From Add!</div>;
+  return (
+    <main className="container mt-3">
+      <h1>Add an Item!</h1>
+    </main>
+  );
 }
